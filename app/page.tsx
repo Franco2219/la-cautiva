d0fad444a0bb2ed9e055d31f0396be5fbc1fac97
Franco2219 <franferro100@gmail.com>
2026-02-26 13:33:29 -0300

    SASA

--- app/page.tsx
+++ app/page.tsx
@@ -186,7 +186,7 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-3 w-3 bg-[#b35a38]"></span>
                 </span>
                 <p className="font-black uppercase tracking-wide text-sm md:text-base">
-                  Ya estan disponibles las Estadisticas! <span className="text-xl md:text-2xl">mira tu historial, conoce a tus rivales y mucho mas!</span>
+                  Ya estan disponibles las Estadisticas!
                 </p>
               </div>
             </div>
