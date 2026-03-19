c0b95834348568fd8f50a3688184416eea3de6db
Franco2219 <franferro100@gmail.com>
2026-03-19 01:00:00 -0300

    sasa

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,7 +39,7 @@ export const getTournamentStyle = (shortName: string) => {
         "monte carlo": "mc", "mc": "mc",
         "s8 500": "s8_500", "super 8 / 500": "s8_500",
         "s8 250": "s8_250", "super 8 / 250": "s8_250",
-        "miami Open 500": "mi_500",
+        "miami Open 500": "mi_500", "mi_500": "mi_500",
     };
     const styleKey = map[key] || "default";
     return TOURNAMENT_STYLES[styleKey] || TOURNAMENT_STYLES["default"];
