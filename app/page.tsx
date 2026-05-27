2b907984c5431c12bcb921053c48f7c1e2ecd644
Franco2219 <franferro100@gmail.com>
2026-05-27 19:39:25 -0300

    sasa

--- app/page.tsx
+++ app/page.tsx
@@ -401,7 +401,7 @@ export default function Home() {
                   <span className="relative inline-flex rounded-full h-3 w-3 bg-[#b35a38]"></span>
                 </span>
                 <p className="font-black uppercase tracking-wide text-sm md:text-base">
-                  Ya estan abiertas las inscripciones para el Roland Garros! Mas de 150 Inscriptos, no te quedes afuera!
+                  Ya estan sorteados los grupos del Roland Garros! Entra para ver con quien te tocó!
                 </p>
               </div>
             </div>
