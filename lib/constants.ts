1758ca81ee027a7ea3bb231089f16bab4483210b
Franco2219 <franferro100@gmail.com>
2026-09-03 19:04:34 -0300

    sasa

--- lib/constants.ts
+++ lib/constants.ts
@@ -45,7 +45,7 @@ export const TOURNAMENT_STYLES: Record<string, TournamentStyle> = {
         logo: "/logos/adelaide.png", 
         pointsLogo: "/logos/pts_s8_500.png" 
     },
-    "washington": { 
+    "wa": { 
       color: "bg-blue-900", 
       borderColor: "border-blue-900", 
       textColor: "text-blue-900", 
